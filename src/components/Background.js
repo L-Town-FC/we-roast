c374143fac15e33c0dedda811e90dec0ef5604fb
atmollohan <atmollohan@gmail.com>
2020-05-04 19:23:34 -0400

    added shared styled componentes

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,40 +1,56 @@
 import React from 'react'
-import { graphql, StaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import styled from 'styled-components'
  
 import BackgroundImage from 'gatsby-background-image'
- 
-const BackgroundSection = ({ className, children }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        desktop: file(relativePath: { eq: "beans/rodrigo-flores.jpg" }) {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid_withWebp
+import { StyledFullScreenWrapper } from './SharedStyledComponents'
+
+/**
+ * In this functional component a fullscreen <BackgroundImage />  is created.
+ * @param className   string    className(s) from styled-components.
+ * @param children    nodes     Child-components.
+ * @return {*}
+ * @constructor
+ */
+const FullBackground = ({ className, children }) => {
+    const { desktop } = useStaticQuery(
+      graphql`
+        query {
+          desktop: file(relativePath: { eq: "beans/rodrigo-flores.jpg" }) {
+            childImageSharp {
+              fluid(quality: 90, maxWidth: 4160) {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
             }
           }
         }
-      }
-    `}
-    render={data => {
-      // Set ImageData.
-      const imageData = data.desktop.childImageSharp.fluid
-      return (
+      `
+    )
+  
+    // Single Image Data
+    const imageData = desktop.childImageSharp.fluid
+  
+    return (
+      <StyledFullScreenWrapper>
         <BackgroundImage
           Tag="section"
           className={className}
           fluid={imageData}
           backgroundColor={`#040e18`}
+          title="Fullscreen Background"
+          id="fullscreenbg"
+          role="img"
+          aria-label="Fullscreen Background"
+          preserveStackingContext={true}
         >
-          
+          {children}
         </BackgroundImage>
-      )
-    }}
-  />
-)
+      </StyledFullScreenWrapper>
+    )
+  } 
+
  
-const StyledBackgroundSection = styled(BackgroundSection)`
+const StyledBackgroundSection = styled(FullBackground)`
   width: 100%;
   background-position: bottom center;
   background-repeat: repeat-y;
