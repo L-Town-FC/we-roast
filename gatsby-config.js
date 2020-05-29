d80dbd6f2667856c8dd425909b6cf5670c61f283
atmollohan <atmollohan@gmail.com>
2020-05-29 19:56:50 -0400

    added new config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,26 +8,28 @@ require("dotenv").config({
     path: `.env.${process.env.NODE_ENV}`,
 })
 
-const contentfulConfig = {
-    spaceId: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+try {
+    var contentfulConfig = require("./.contentful.json")
+} catch (e) {
+    var contentfulConfig = {
+        production: {
+            spaceId: process.env.SPACE_ID,
+            accessToken: process.env.ACCESS_TOKEN,
+        },
+    }
 }
+
+const contentfulOptions =
+    process.env.NODE_ENV === "development"
+        ? contentfulConfig.development
+        : contentfulConfig.production
+
 
 // if you want to use the preview API please define
 // CONTENTFUL_HOST in your environment config
 // the `host` property should map to `preview.contentful.com`
 // https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
-if (process.env.CONTENTFUL_HOST) {
-    contentfulConfig.host = process.env.CONTENTFUL_HOST
-}
 
-const { spaceId, accessToken } = contentfulConfig
-
-if (!spaceId || !accessToken) {
-    throw new Error(
-        "Contentful spaceId and the access token need to be provided."
-    )
-}
 
 module.exports = {
     /* Your site config here */
@@ -131,8 +133,13 @@ module.exports = {
         `gatsby-transformer-sharp`,
         {
             resolve: "gatsby-source-contentful",
-            options: contentfulConfig,
+            options: contentfulOptions,
         },
+        {
+            resolve: "contentful-editor",
+            options: contentfulOptions,
+        },
+        `gatsby-plugin-netlify`,
         `gatsby-remark-images`,
         {
             resolve: `gatsby-plugin-mdx`,
